bfbf22ef6a08182959508bc5fb1109624abae98f
Danny Hadley <dadley@sizethreestudios.com>
2013-10-06 21:33:34 -0400

    added a few basic grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,28 @@ var grunt = require('grunt');
 
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-smash');
+
   grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      build: ['build']
+    },
+
+    smash: { 
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.js'
+      }
+    }
 
   });
 
-  grunt.registerTask('build', [ ]);
+  grunt.registerTask('build', ['clean', 'smash']);
   grunt.registerTask('default',[ ]);
 
 };
