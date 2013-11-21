d3ca84e6de2c9d71f5376b06d5a6125d5f1684e6
Danny Hadley <dadley@sizethreestudios.com>
2013-11-21 00:17:29 -0500

    added connect-related dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-smash');
 
